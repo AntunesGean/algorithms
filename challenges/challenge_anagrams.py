0fb02f7ba6c7bbcbaaa3bd69a14ce594694abd95
AntunesGean <gean.antunes@gmail.com>
2023-03-03 11:18:36 -0300

    req 04 - ok

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,5 +1,44 @@
-# iniciando o projeto
+def join_sort(arr_word):
+
+    if len(arr_word) <= 1:
+        return arr_word
+
+    middle = len(arr_word) // 2
+
+    left = join_sort(arr_word[:middle])
+    right = join_sort(arr_word[middle:])
+
+    return join_merge(left, right)
+
+
+def join_merge(left, right):
+
+    arr = []
+    counterA = 0
+    counterB = 0
+
+    while counterA < len(left) and counterB < len(right):
+
+        if left[counterA] < right[counterB]:
+            arr.append(left[counterA])
+            counterA += 1
+        else:
+            arr.append(right[counterB])
+            counterB += 1
+
+    arr += left[counterA:]
+    arr += right[counterB:]
+
+    return "".join(arr)
 
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+
+    first_Lower = first_string.lower()
+    second_Lower = second_string.lower()
+
+    first = join_sort(first_Lower)
+    second = join_sort(second_Lower)
+
+    isTrue = (first != "" and second != "" and first == second)
+
+    return (first, second, isTrue)

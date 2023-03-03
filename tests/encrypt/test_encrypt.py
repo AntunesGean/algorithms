eb42dc2a299b95b2d812c28423b7cf795a0aed11
AntunesGean <gean.antunes@gmail.com>
2023-03-03 10:37:32 -0300

    req 02 - ok

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,12 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+
+    assert encrypt_message("abcabc", 3) == "cba_cba"
+    assert encrypt_message("abcabc", 4) == "cb_acba"
+    assert encrypt_message("abcabc", 9) == "cbacba"
+
+    with pytest.raises(TypeError):
+        encrypt_message(3, "abcabc")
